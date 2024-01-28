9fa679359113d9eabfa51ab0459b5e7462690d72
harkanni <harkanzyempire001@gmail.com>
2024-01-28 22:43:28 +0100

    init test

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,19 @@ export default function Home() {
    return (
       <main className="bg-slate-800/40 h-[100vh] flex flex-col justify-center items-center">
          <form action="" className="border p-5 rounded-lg flex flex-col gap-3 w-[500px]">
-            <div className="formGroup flex">
+            <div className="formGroup flex rounded-md">
                <div className="inputIcon bg-slate-950 p-5">
                   <FaUser size={25} />
                </div>
-               <input type="text" placeholder="Username" className="w-[100%] pl-5 text-black" />
+               <input type="text" placeholder="Username" className="w-[100%] pl-5 text-black removeFocused" />
             </div>
-            <div className="formGroup flex">
+            <div className="formGroup flex rounded-md">
                <div className="inputIcon bg-slate-950 p-5">
                   <FaLock size={25} />
                </div>
                <input type="password" placeholder="Password" className="w-[100%] pl-5 text-black" />
             </div>
-            <button className="p-4" style={{ backgroundColor: '#e74c87' }}>SIGN IN</button>
+            <button className="p-4 rounded-md" style={{ backgroundColor: '#e74c87' }}>SIGN IN</button>
          </form>
          <p>Not a member ? <span>Sign up now</span></p>
       </main>
