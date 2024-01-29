4649c67ea5cbd09ff6092346ef9688bbad9edb71
harkanni <harkanzyempire001@gmail.com>
2024-01-29 16:13:31 +0100

    password

--- app/page.tsx
+++ app/page.tsx
@@ -8,64 +8,91 @@ import { useState } from "react";
 
 export default function Home() {
    const [selectedCard, setSelectedCard]: any = useState([])
-   const [password, setPassword] = useState()
+   const [password, setPassword] = useState([1, 2, 3, 4])
+   const [passwordIsCorrect, setPasswordIsCorrect] = useState<boolean>(true)
+   const [cardViscible, setCardViscible] = useState(false)
 
    const handleClick = (id: number) => {
-      !selectedCard.includes(id) ? setSelectedCard([...selectedCard, id])
-         : setSelectedCard(selectedCard.filter((cardId :number) => cardId !== id)) ;
-      console.log(selectedCard, id)
-      selectedCard.includes(id) ? console.log(true) : console.log(false)
-   }
+      const updatedSelectedCards = selectedCard.includes(id)
+         ? selectedCard.filter((cardId: number) => cardId !== id)
+         : [...selectedCard, id];
+
+      setSelectedCard(updatedSelectedCards);
+      console.log(updatedSelectedCards, id);
+
+      if (updatedSelectedCards.length >= 4) {
+         if (validatePassword(updatedSelectedCards)) {
+            console.log('Password validated successfully!');
+         } else {
+            console.log('Password validation failed!');
+         }
+      }
+   };
+
+   const validatePassword = (selectedCards: number[]) => {
+      for (let i = 0; i < selectedCards.length; i++) {
+         if (selectedCards[i] !== password[i]) {
+            setPasswordIsCorrect(false)
+            setSelectedCard([])
+            return false;
+         }
+      }
+      setPasswordIsCorrect(true)
+      setSelectedCard([])
+      return true;
+   };
+
 
 
 
    return (
-      <main className="bg-slate-800/40 h-[100vh] flex flex-col justify-center items-center">
+      <main className="bg-slate-800/40 min-h-[100vh] flex flex-col justify-center items-center">
          <form action="" className="border p-5 rounded-lg flex flex-col gap-3 w-[600px]">
             <div className="formGroup flex rounded-md">
                <div className="inputIcon bg-slate-950 p-5">
                   <FaUser size={25} />
                </div>
-               <input autoComplete={'false'} type="text" placeholder="Username" className="w-[100%] pl-5 text-black removeFocused" />
+               <input autoComplete="off" type="text" placeholder="Username" className="w-[100%] pl-5 text-black removeFocused" />
             </div>
             <div className="formGroup flex rounded-md">
                <div className="inputIcon bg-slate-950 p-5">
                   <FaLock size={25} />
                </div>
-               <input autoComplete="false" type="password" placeholder="Password" className="w-[100%] pl-5 text-black" />
-            </div>
-            <div className="flex items-end gap-2 w-[100%] h-[50vh]">
-               <div className={`w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(1) ? 'selected' : ''}`} onClick={() => handleClick(1)}>
-                  <Image
-                     src={cardImage}
-                     alt="Card Image"
-                     className='rounded-md h-[100%] w-[100%]'
-                  />
-               </div>
-               <div className={`w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(2) ? 'selected' : ''}`} onClick={() => handleClick(2)}>
-                  <Image
-                     src={cardImage}
-                     alt="Card Image"
-                     className='rounded-md h-[100%] w-[100%]'
-                  />
-               </div>
-               <div className={`w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(3) ? 'selected' : ''}`} onClick={() => handleClick(3)}>
-                  <Image
-                     src={cardImage}
-                     alt="Card Image"
-                     className='rounded-md h-[100%] w-[100%]'
-                  />
-               </div>
-               <div className={`w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(4) ? 'selected' : ''}`} onClick={() => handleClick(4)}>
-                  <Image
-                     src={cardImage}
-                     alt="Card Image"
-                     className='rounded-md h-[100%] w-[100%]'
-                  />
-               </div>
+               <input name="32" autoComplete="off" type="password" placeholder="Password" className="w-[100%] pl-5 text-black" onFocus={() => setCardViscible(true)} />
             </div>
             <button className="p-4 rounded-md" style={{ backgroundColor: '#e74c87' }}>SIGN IN</button>
          </form>
+
+         <div className={`${cardViscible ? 'flex' : 'hidden opacity-0'} items-end gap-2 w-[600px] h-[50vh] transition-all cardContainer`}>
+            <div className={`${passwordIsCorrect ? 'correct' : 'error'} w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(1) ? 'selected' : ''}`} onClick={() => handleClick(1)}>
+               <Image
+                  src={cardImage}
+                  alt="Card Image"
+                  className={`rounded-md h-[100%] w-[100%]`}
+               />
+            </div>
+            <div className={`${passwordIsCorrect ? 'correct' : 'error'} w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(2) ? 'selected' : ''}`} onClick={() => handleClick(2)}>
+               <Image
+                  src={cardImage}
+                  alt="Card Image"
+                  className='rounded-md h-[100%] w-[100%]'
+               />
+            </div>
+            <div className={`${passwordIsCorrect ? 'correct' : 'error'} w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(3) ? 'selected' : ''}`} onClick={() => handleClick(3)}>
+               <Image
+                  src={cardImage}
+                  alt="Card Image"
+                  className='rounded-md h-[100%] w-[100%]'
+               />
+            </div>
+            <div className={`${passwordIsCorrect ? 'correct' : 'error'} w-[25%] h-[80%] cursor-pointer ${selectedCard.includes(4) ? 'selected' : ''}`} onClick={() => handleClick(4)}>
+               <Image
+                  src={cardImage}
+                  alt="Card Image"
+                  className='rounded-md h-[100%] w-[100%]'
+               />
+            </div>
+         </div>
          <p>Not a member ? <span>Sign up now</span></p>
       </main>
    );
